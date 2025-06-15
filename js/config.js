a4c9625d88b4103ac3c894a2fc1b7b79baeb48ee
balaji38738 <39882654+balaji38738@users.noreply.github.com>
2025-06-15 06:46:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_uRe4FniZf', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7a24c2an1p75u86d0pala854js', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
